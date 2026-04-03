bff13a9e1f1174445a97d9ce710a72527eb92adf
MASIBODAN <MASIBODAN@DESKTOP-5U981U0>
2026-04-04 00:58:59 +0300

    dddownload only

---
+++ wwwroot/Site.js
@@ -0,0 +1,19 @@
+﻿window.downloadPageAsPdf = async function (elementId, filename) {
+    const { jsPDF } = window.jspdf;
+
+    const element = document.getElementById(elementId);
+    if (!element) {
+        console.error("Element not found: " + elementId);
+        return;
+    }
+
+    const canvas = await html2canvas(element, { scale: 2 });
+    const imgData = canvas.toDataURL("image/png");
+
+    const pdf = new jsPDF("p", "mm", "a4");
+    const pageWidth = pdf.internal.pageSize.getWidth();
+    const imgHeight = (canvas.height * pageWidth) / canvas.width;
+
+    pdf.addImage(imgData, "PNG", 0, 0, pageWidth, imgHeight);
+    pdf.save(filename);
+};
